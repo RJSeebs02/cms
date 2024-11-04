88da0a3abff21b258799bc1ce19ac5ce38df2527
Romeo V. Seva III <100087338+RJSeebs02@users.noreply.github.com>
2024-11-04 13:54:19 +0800

    Add files via upload

---
+++ cms/activity/dashboard.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+    <h2>hello</h2>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="assets/styles.css" />
+</head>
+<body>
+    <div id="wrapper">
+        <div class="header">
+
+        </div>
+        <div class="wrapper">
+
+            </div>
+        </div>
+    </body>
+</html>
